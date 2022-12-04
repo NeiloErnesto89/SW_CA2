83bd733497c3b23ba3dd740716d1a66bc421aec0
NeiloErnesto89 <neil.smyth1989@hotmail.com>
2022-12-04 01:01:17 +0000

    updated GenericStack, added constructors and used IList to push given element (via add() element) to top of stack. Moved onto pop & peek methods.

--- src/main/java/com/generics/GenericStack.java
+++ src/main/java/com/generics/GenericStack.java
@@ -1,13 +1,34 @@
 package com.generics;
 
-public class GenericStack<T> implements IStack<T> {
+//import java.util.Iterator;
+
+import java.util.Iterator;
+
+public class GenericStack<T> implements IStack<T> { //, Iterable<T>
+
+    private IList<T> stackData; //via requirement notes
+
+    /* default constructor */
+    public GenericStack() {
+        stackData = new GenericArrayList<>(); //new genericAL obj
+    }
+    public GenericStack(IList<T> data) {
+        stackData = data; // via generic IList
+    }
+
     @Override
     public void push(T element) {
+
+        stackData.add(element); // 'push' or add element to the top of the stack
 
     }
 
     @Override
     public T pop() {
+        if (!empty()) {
+            return null;
+        }
+
         return null;
     }
 
@@ -20,4 +41,12 @@ public class GenericStack<T> implements IStack<T> {
     public boolean empty() {
         return false;
     }
+
+//    /* Returns an iterator over the elements in this list in proper sequence.*/
+//    @Override
+//    public Iterator<T> iterator() {
+//
+////        return new GenericLinkedList.GenericLinkedListIterator();
+//        return new GenericStackIterator<>()
+//    }
 }
